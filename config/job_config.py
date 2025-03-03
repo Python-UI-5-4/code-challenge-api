296f7e9403210dcb8945bf115fcfbfbf3d3d9c12
Lee Sangwon <83111797+swon2id@users.noreply.github.com>
2025-03-03 20:02:02 +0900

    feat: 작업 허용 언어 'python3' 추가

--- config/job_config.py
+++ config/job_config.py
@@ -4,9 +4,10 @@ class JobConfig:
         코드 채점 작업 설정 관련 설정(동시에 유지 가능한 코드 채점 job 갯수 제한 등)을 관리하는 클래스.
     """
     MAX_JOB_COUNT_PER_USER = 2
-    VALID_LANGUAGE = {'java17', 'nodejs20', 'nodejs20esm'}
+    VALID_LANGUAGE = {'java17', 'nodejs20', 'nodejs20esm', 'python3'}
     LANGUAGE_EXEC_TIME_EXTRA_SEC = {
         'java17': 1.0,
         'nodejs20': 0.0,
-        'nodejs20esm': 0.0
+        'nodejs20esm': 0.0,
+        'python3': 0.0,
     }
